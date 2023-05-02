1a139b8dafcc30f80ab52af7591b27b5e9597012
synt3tic <mel04nax@gmail.com>
2023-05-02 11:45:37 +0300

    header ready

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const navigationItems = [
+  {
+    id: 1,
+    name: 'Тренажёр',
+    routeName: 'train',
+  },
+  {
+    id: 2,
+    name: 'Статистика',
+    routeName: 'statistic',
+  },
+];
+</script>
+
+<template>
+  <nav class="navigation">
+    <div
+      v-for="item in navigationItems"
+      :key="item.id"
+      class="navigation__item"
+    >
+      <p>{{ item.name }}</p>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navigation {
+  @apply flex items-center h-full;
+}
+.navigation__item {
+  @apply flex items-center h-full px-2 text-white transition-all hover:cursor-pointer hover:bg-blue-300;
+}
+</style>
